6f1173498066d9ae359752a975d5c85419eb371f
lulukarissa <lulukarissa@gmail.com>
2019-03-05 15:13:16 +0700

    correction

--- src/App.js
+++ src/App.js
@@ -11,22 +11,23 @@ class App extends Component {
   render() {
     var klik = () =>{
       var nama = this.refs.isi.value
-      var url = `https://robohash.org/${nama}`
+      var number = Math.floor(Math.random() * 4) + 1 //min 1 max 4
+      var url = `https://robohash.org/${nama}.png?set=set${number}`
 
       this.setState({
         generator: url
       })
     }
     return (
-      
-      <div className="mr-md-3">
+      <div className="mr-md-3" style={{marginTop:'30px'}}>
         <center>
           <div class="col-sm-6">
             <div class="card">
               <div class="card-body">
+                <h2 style={{color:'green'}}>Profile Picture Generator</h2><br/>
                 <input type="text" ref="isi" placeholder="Ketik nama anda"></input><br/><br/>
                 <button onClick={klik} class="btn btn-success">Buat Profile Picture!</button><br/><br/>
-                <img src={this.state.generator}></img>
+                <img src={this.state.generator} style={{borderRadius:'100%'}} alt=''></img>
               </div>
             </div>
           </div>
